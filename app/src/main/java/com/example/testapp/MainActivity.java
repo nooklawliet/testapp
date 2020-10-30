4c8d6d6df00970a3e01fc129d3bd98053b553e6b
nooklawliet <nooklawliet@gmail.com>
2020-10-30 11:52:47 +0700

    implement

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.testapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.calculator.Calculator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Calculator cal = new Calculator();
+//        int sum = cal.sum(10, 20);
+//        Log.d("TAG", "sum: " + sum);
+
     }
 }
